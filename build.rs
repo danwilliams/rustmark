3efca8d386b5d82555b39a831176bb81141fe06d
Dan Williams <dan@dotfive.co.uk>
2023-06-11 16:47:14 +0100

    Separated out functionality in build script
    Separated out the Markdown parsing and file copying functionality in the
    build script into parse() and copy() functions, ready for making them
    run asynchronously.

--- build.rs
+++ build.rs
@@ -46,85 +46,95 @@ fn main() {
 			}
 			continue;
 		}
-		//		Copy non-Markdown files											
+		//		Handle files													
 		if input_path.extension().is_none() || input_path.extension().unwrap() != "md" {
-			//	Normally we might want to use hardlinks in order to save space, but the
-			//	way that cargo build works is to create a new directory every time. It
-			//	might therefore be confusing to use hardlinks given that changes would
-			//	then pollute the build directories, negating Cargo's intent of being
-			//	able to look at any build directory to see exactly what was built for
-			//	that particular build.
-			println!("Copying file: {} -> {}", input_path.display(), output_path.display());
-			fs::copy(input_path, output_path).unwrap();
-			continue;
+			copy(&input_path, &output_path);
+		} else {
+			parse(&input_path, &output_path);
 		}
-		//		Parse Markdown files											
-		//	Under different circumstances, i.e. in other build systems, we might
-		//	want to keep track of file modification times, and only re-parse files
-		//	that have been modified since the last build. However, cargo build
-		//	always creates a new build directory, so we have to re-parse everything.
-		//	Although this does take more time, it fits with Cargo's intent of being
-		//	able to look at any build directory and see exactly what was built for
-		//	that particular build.
-		println!("Parsing file: {}", input_path.display());
-		let adaptor     = SyntectAdapter::new("base16-ocean.dark");
-		let mut plugins = ComrakPlugins::default();
-		plugins.render.codefence_syntax_highlighter = Some(&adaptor);
-		let html        = markdown_to_html_with_plugins(
-			&fs::read_to_string(&input_path).unwrap(),
-			&ComrakOptions {
-				extension:                     ComrakExtensionOptions {
-					strikethrough:             true,
-					tagfilter:                 true,
-					table:                     true,
-					autolink:                  true,
-					tasklist:                  true,
-					superscript:               true,
-					header_ids:                Some("".to_owned()),
-					footnotes:                 true,
-					description_lists:         true,
-					front_matter_delimiter:    Some("---".to_owned()),
-					shortcodes:                true,
-				},
-				parse:                         ComrakParseOptions {
-					smart:                     true,
-					default_info_string:       Some("".to_owned()),
-					relaxed_tasklist_matching: true,
-				},
-				render:                        ComrakRenderOptions {
-					hardbreaks:                false,
-					github_pre_lang:           false,
-					full_info_string:          true,
-					width:                     80,
-					unsafe_:                   true,
-					escape:                    false,
-					list_style:                ListStyleType::Dash,
-					sourcepos:                 false,
-				},
-			},
-			&plugins,
-		);
-		//		Interrogate HTML												
-		//	We'll use the first h1 element as the title of the page, but only if the
-		//	first H1 is the first element in the document. If any other content
-		//	comes before it, we won't count it as being the title.
-		let mut document =  Html::parse_document(&html);
-		let (title, id)  =  match document.select(&Selector::parse("h1:first-child").unwrap()).next() {
-			Some(h1)     => (h1.text().collect::<String>(), Some(h1.id())),
-			None         => ("Untitled".to_owned(), None),
-		};
-		//	Remove the title from the index page, as it will have one added showing
-		//	the application title.
-		if input_path == Path::new("content/index.md") {
-			if let Some(id) = id { document.tree.get_mut(id).unwrap().detach() }
-		}
-		//		Write output													
-		//	We use a custom format - the first line of the file is the title we
-		//	extracted, and the rest is the HTML.
-		let mut output_file = File::create(&output_path).unwrap();
-		output_file.write_all(format!("{}\n", &title).as_bytes()).unwrap();
-		output_file.write_all(document.html().as_bytes()).unwrap();
 	}
+}
+
+//		copy																	
+fn copy(input_path: &Path, output_path: &Path) {
+	//	Normally we might want to use hardlinks in order to save space, but the
+	//	way that cargo build works is to create a new directory every time. It
+	//	might therefore be confusing to use hardlinks given that changes would
+	//	then pollute the build directories, negating Cargo's intent of being
+	//	able to look at any build directory to see exactly what was built for
+	//	that particular build.
+	println!("Copying file: {} -> {}", input_path.display(), output_path.display());
+	fs::copy(input_path, output_path).unwrap();
+}
+
+//		parse																	
+fn parse(input_path: &Path, output_path: &Path) {
+	//		Parse Markdown														
+	//	Under different circumstances, i.e. in other build systems, we might
+	//	want to keep track of file modification times, and only re-parse files
+	//	that have been modified since the last build. However, cargo build
+	//	always creates a new build directory, so we have to re-parse everything.
+	//	Although this does take more time, it fits with Cargo's intent of being
+	//	able to look at any build directory and see exactly what was built for
+	//	that particular build.
+	println!("Parsing file: {}", input_path.display());
+	let adaptor     = SyntectAdapter::new("base16-ocean.dark");
+	let mut plugins = ComrakPlugins::default();
+	plugins.render.codefence_syntax_highlighter = Some(&adaptor);
+	let html        = markdown_to_html_with_plugins(
+		&fs::read_to_string(input_path).unwrap(),
+		&ComrakOptions {
+			extension:                     ComrakExtensionOptions {
+				strikethrough:             true,
+				tagfilter:                 true,
+				table:                     true,
+				autolink:                  true,
+				tasklist:                  true,
+				superscript:               true,
+				header_ids:                Some("".to_owned()),
+				footnotes:                 true,
+				description_lists:         true,
+				front_matter_delimiter:    Some("---".to_owned()),
+				shortcodes:                true,
+			},
+			parse:                         ComrakParseOptions {
+				smart:                     true,
+				default_info_string:       Some("".to_owned()),
+				relaxed_tasklist_matching: true,
+			},
+			render:                        ComrakRenderOptions {
+				hardbreaks:                false,
+				github_pre_lang:           false,
+				full_info_string:          true,
+				width:                     80,
+				unsafe_:                   true,
+				escape:                    false,
+				list_style:                ListStyleType::Dash,
+				sourcepos:                 false,
+			},
+		},
+		&plugins,
+	);
+	//		Interrogate HTML													
+	//	We'll use the first h1 element as the title of the page, but only if the
+	//	first H1 is the first element in the document. If any other content
+	//	comes before it, we won't count it as being the title.
+	let mut document =  Html::parse_document(&html);
+	let (title, id)  =  match document.select(&Selector::parse("h1:first-child").unwrap()).next() {
+		Some(h1)     => (h1.text().collect::<String>(), Some(h1.id())),
+		None         => ("Untitled".to_owned(), None),
+	};
+	//	Remove the title from the index page, as it will have one added showing
+	//	the application title.
+	if input_path == Path::new("content/index.md") {
+		if let Some(id) = id { document.tree.get_mut(id).unwrap().detach() }
+	}
+	//		Write output														
+	//	We use a custom format - the first line of the file is the title we
+	//	extracted, and the rest is the HTML.
+	let mut output_file = File::create(output_path).unwrap();
+	output_file.write_all(format!("{}\n", &title).as_bytes()).unwrap();
+	output_file.write_all(document.html().as_bytes()).unwrap();
 }
 
 
